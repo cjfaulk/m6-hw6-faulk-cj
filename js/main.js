4a619255f8f88d37a366048fbf0c192af375434c
CJ Faulk <cjfaulk@ufl.edu>
2020-10-09 21:13:00 -0400

    Attempt part i

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Part i: hover highlights boxes with a different background color
+
+document.getElementById("one").addEventListener("mouseenter", boxBackground);
+document.getElementById("two").addEventListener("mouseenter", boxBackground);
+document.getElementById("three").addEventListener("mouseenter", boxBackground);
+
+function boxBackground() {
+  this.style.backgroundColor = "#FFA500";
+  return false;
+};
+
+
+
+
+
+// Part ii: box #1 click
+
+
+
+// Part iii: box #2 click
+
+
+
+// Part iv: box #3 click
+
+
+
+// Part v: start over button
